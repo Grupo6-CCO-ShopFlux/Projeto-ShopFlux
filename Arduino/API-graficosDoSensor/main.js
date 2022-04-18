76ea134ed703461779e5a18a9da6234b56341f31
KauanCavazaniBrianez <kauan.brianez@sptech.school>
2022-04-18 13:02:58 -0300

    API sensores

---
+++ Arduino/API-graficosDoSensor/main.js
@@ -0,0 +1,137 @@
+/* Importa as libraries que vão ser usadas no código */
+const serialport = require('serialport');
+const express = require('express');
+const mysql = require('mysql2');
+
+/* Define as variáveis const */
+const SERIAL_BAUD_RATE = 9600;
+const SERVIDOR_PORTA = 3000;
+const HABILITAR_OPERACAO_INSERIR = true;
+
+/* Função que faz a leitura dos dados do serial port*/
+const serial = async (
+    valoresDht11Umidade,
+    valoresDht11Temperatura,
+    valoresLuminosidade,
+    valoresLm35Temperatura,
+    valoresChave
+) => {
+    /* Cria uma conexão com o banco de dados */
+    const poolBancoDados = mysql.createPool(
+        {
+            host: 'localhost',
+            port: 3306,
+            user: 'aluno',
+            password: 'sptech',
+            database: 'sensor'
+        }
+    ).promise();
+
+    /* Procura pela porta do arduino */
+    const portas = await serialport.SerialPort.list();
+    const portaArduino = portas.find((porta) => porta.vendorId == 2341 && porta.productId == 43);
+
+    /* Verifica se o arduino está conectado com o computador */
+    if (!portaArduino) {
+        throw new Error('O arduino não foi encontrado em nenhuma porta serial');
+    }
+    
+    const arduino = new serialport.SerialPort(
+        {
+            path: portaArduino.path,
+            baudRate: SERIAL_BAUD_RATE
+        }
+    );
+    /* Printa uma mensagem no console log quando a leitura do arduino for iniciada */
+    arduino.on('open', () => {
+        console.log(`A leitura do arduino foi iniciada na porta ${portaArduino.path} utilizando Baud Rate de ${SERIAL_BAUD_RATE}`);
+    });
+    /* Faz a leitura dos dados do serial port e salva em arrays */
+    arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
+        const valores = data.split(';');
+        const dht11Umidade = parseFloat(valores[0]);
+        const dht11Temperatura = parseFloat(valores[1]);
+        const luminosidade = parseFloat(valores[2]);
+        const lm35Temperatura = parseFloat(valores[3]);
+        const chave = parseInt(valores[4]);
+
+        /* Envia os valores dos sensores dentro do array */
+        valoresDht11Umidade.push(dht11Umidade);
+        valoresDht11Temperatura.push(dht11Temperatura);
+        valoresLuminosidade.push(luminosidade);
+        valoresLm35Temperatura.push(lm35Temperatura);
+        valoresChave.push(chave);
+
+        /* Inseri os valores no banco de dados */
+        if (HABILITAR_OPERACAO_INSERIR) {
+            await poolBancoDados.execute(
+                'INSERT INTO sensores (dht11_umidade, dht11_temperatura, luminosidade, lm35_temperatura, chave) VALUES (?, ?, ?, ?, ?)',
+                [dht11Umidade, dht11Temperatura, luminosidade, lm35Temperatura, chave]
+            );
+        }
+
+    });
+    /* Printa uma mensagem de erro no console log */
+    arduino.on('error', (mensagem) => {
+        console.error(`Erro no arduino (Mensagem: ${mensagem}`)
+    });
+}
+
+/* Uma função que recebe os valores dos sensores e retorna em forma de JSON. */
+const servidor = (
+    valoresDht11Umidade,
+    valoresDht11Temperatura,
+    valoresLuminosidade,
+    valoresLm35Temperatura,
+    valoresChave
+) => {
+    const app = express();
+    app.use((request, response, next) => {
+        response.header('Access-Control-Allow-Origin', '*');
+        response.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+        next();
+    });
+    app.listen(SERVIDOR_PORTA, () => {
+        console.log(`API executada com sucesso na porta ${SERVIDOR_PORTA}`);
+    });
+    app.get('/sensores/dht11/umidade', (_, response) => {
+        return response.json(valoresDht11Umidade);
+    });
+    app.get('/sensores/dht11/temperatura', (_, response) => {
+        return response.json(valoresDht11Temperatura);
+    });
+    app.get('/sensores/luminosidade', (_, response) => {
+        return response.json(valoresLuminosidade);
+    });
+    app.get('/sensores/lm35/temperatura', (_, response) => {
+        return response.json(valoresLm35Temperatura);
+    });
+    app.get('/sensores/chave', (_, response) => {
+        return response.json(valoresChave);
+    });
+}
+
+/* Cria um array para cada sensor */
+(async () => {
+    const valoresDht11Umidade = [];
+    const valoresDht11Temperatura = [];
+    const valoresLuminosidade = [];
+    const valoresLm35Temperatura = [];
+    const valoresChave = [];
+    /* Chama a função serial e envia os dados como parametro */
+    await serial(
+        valoresDht11Umidade,
+        valoresDht11Temperatura,
+        valoresLuminosidade,
+        valoresLm35Temperatura,
+        valoresChave
+    );
+    /* Chama a função servidor e envia os dados como parametro */
+    servidor(
+        valoresDht11Umidade,
+        valoresDht11Temperatura,
+        valoresLuminosidade,
+        valoresLm35Temperatura,
+        valoresChave
+    );
+})();
