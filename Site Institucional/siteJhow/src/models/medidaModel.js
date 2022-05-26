49cfb89912d16e9da925869ca433b1ae2117e403
KauanCavazaniBrianez <kauan.brianez@sptech.school>
2022-05-26 12:10:52 -0300

    configurando graficos

--- Site Institucional/siteJhow/src/models/medidaModel.js
+++ Site Institucional/siteJhow/src/models/medidaModel.js
@@ -85,6 +85,60 @@ function buscarUltimasMedidas(idGrafico, limite_linhas, tipoGrafico) {
             console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
             return
         }
+    } else if(tipoGrafico == 'mensal') {
+        if (process.env.AMBIENTE_PROCESSO == "producao") {
+            instrucaoSql = `select top ${limite_linhas}
+                            chave as setor1, 
+                            chave as setor2,
+                            chave as setor3,
+                            chave as setor4,
+                            momento,
+                            CONVERT(varchar, momento, 108) as momento_grafico
+                        from medida
+                        where fkGrafico = ${idGrafico}
+                        order by idMedida desc`;
+        } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+            instrucaoSql = `select 
+                            (chave + 15000) as setor1, 
+                            (chave + 25000) as setor2,
+                            (chave + 30000) as setor3,
+                            (chave + 8000) as setor4,
+                            momento,
+                            DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
+                        from medida
+                        where fkGrafico = ${idGrafico}
+                        order by idMedida desc limit ${limite_linhas}`;
+        } else {
+            console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+            return
+        }
+    } else if(tipoGrafico == 'anual') {
+        if (process.env.AMBIENTE_PROCESSO == "producao") {
+            instrucaoSql = `select top ${limite_linhas}
+                            chave as setor1, 
+                            chave as setor2,
+                            chave as setor3,
+                            chave as setor4,
+                            momento,
+                            CONVERT(varchar, momento, 108) as momento_grafico
+                        from medida
+                        where fkGrafico = ${idGrafico}
+                        order by idMedida desc`;
+        } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+            instrucaoSql = `select 
+                            (chave + 15000) as setor1, 
+                            (chave + 25000) as setor2,
+                            (chave + 30000) as setor3,
+                            (chave + 8000) as setor4,
+                            momento,
+                            DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
+                        from medida
+                        where fkGrafico = ${idGrafico}
+                        order by idMedida desc limit ${limite_linhas}`;
+        } else {
+            console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+            return
+        }
     }
     
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
